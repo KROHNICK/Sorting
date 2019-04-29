dce2a84aaf639499e596af01e9cdb0e0001c058a
KROHNICK <business.kanekperez@gmail.com>
2019-04-29 14:13:07 -0700

    completed bubble sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -24,11 +24,19 @@ def selection_sort(arr):
 
 
 def bubble_sort(arr):
-
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
 
 
+# print(bubble_sort(arr=[1, 5, 8, 4, 2, 9, 6, 0, 3, 7]))
+
 # STRETCH: implement the Count Sort function below
+
+
 def count_sort(arr, maximum=-1):
 
     return arr
